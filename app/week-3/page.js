298ccb00ac6928c0487f6cc91dc02a91c99e6ad9
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-01-28 16:21:17 -0700

    styles

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -19,6 +19,7 @@ const style = {
   heading: {
     fontSize: '3rem',
     marginBottom: '2rem',
+    marginTop: '2rem',
     color: '#FFD700', // Yellow text
     textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', // Adds depth
     WebkitTextStroke: '2px black', // Black outline
