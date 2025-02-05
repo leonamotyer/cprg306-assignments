a239ebaac7cc88715b6de1919f0473006be00094
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-02-04 17:03:08 -0700

    maybe

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,5 +1,5 @@
 "use client";
-import Item from 'item';
+import Item from 'item.js';
 
 const item1 = { name: "milk, 4 L 🥛", quantity: 1, category: "dairy" };
 const item2 = { name: "bread 🍞", quantity: 2, category: "bakery" };
