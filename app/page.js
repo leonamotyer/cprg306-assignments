6183a226ac8447159011f89a35a7e3aa0ab0b256
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2025-02-25 17:25:41 -0700

    capital week -6

--- app/page.js
+++ app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
           <a href="/week-5" style={styles.link}>Week 5</a>
         </li>
         <li style={styles.linkItem}>
-          <a href="/week-6" style={styles.link}>Week 6</a>
+          <a href="/Week-6" style={styles.link}>Week 6</a>
         </li>
         <li style={styles.linkItem}>
           <a href="/week-7" style={styles.link}>Week 7</a>
